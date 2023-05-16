9515898e661166ade0a711e73dfa168550dd9d60
AdrianaRoza <adriana.roza.aka@gmail.com>
2023-05-16 16:27:05 -0300

    Initial commit

---
+++ app/src/main/java/com/adriana/diceroller/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.adriana.diceroller
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.adriana.diceroller.ui.theme.DiceRollerTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            DiceRollerTheme{
+                DiceRollerApp()
+            }
+        }
+    }
+}
+@Preview
+@Composable
+fun DiceRollerApp(){
+    DiceWithButtonAndImage()
+}
+
+@Preview
+@Composable
+fun DiceWithButtonAndImage(modifier: Modifier = Modifier
+    .fillMaxSize()
+    .wrapContentSize(Alignment.Center)
+
+) {
+}
