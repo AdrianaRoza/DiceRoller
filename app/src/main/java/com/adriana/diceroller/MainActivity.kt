a29fe3808654bc550368613f7b721575d88420b8
AdrianaRoza <adriana.roza.aka@gmail.com>
2023-05-16 16:59:09 -0300

    feat: add a button

--- app/src/main/java/com/adriana/diceroller/MainActivity.kt
+++ app/src/main/java/com/adriana/diceroller/MainActivity.kt
@@ -4,11 +4,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material.Button
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.adriana.diceroller.ui.theme.DiceRollerTheme
 
@@ -31,8 +32,12 @@ fun DiceRollerApp(modifier: Modifier = Modifier){
 @Preview
 @Composable
 fun DiceWithButtonAndImage(modifier: Modifier = Modifier) {
-    Column (
+    Column(
         modifier = modifier,
         horizontalAlignment = Alignment.CenterHorizontally
-    ) {}
+    ) {
+        Button(onClick = { /*TODO*/ }) {
+            Text(stringResource(R.string.roll))
+        }
+    }
 }
